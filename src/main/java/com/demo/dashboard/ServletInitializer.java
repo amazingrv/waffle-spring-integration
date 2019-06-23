873c84230d33e3b9c07bf83140847563bcfec7a4
Rajat Verma <i.rajatverma21@gmail.com>
2019-06-23 19:19:48 +0530

    Update pom and servlet initlizer

--- src/main/java/com/demo/dashboard/ServletInitializer.java
+++ src/main/java/com/demo/dashboard/ServletInitializer.java
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(DashDemoApplication.class);
+		return application.sources(WaffleApplication.class);
 	}
 
 }
